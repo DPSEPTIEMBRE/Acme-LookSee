96d48b436eab2cb1936d2a6f2f2d777e81b0bc08
LauraSmile <laura94sev@hotmail.com>
2017-07-18 12:19:42 +0200

    Services

--- src/main/java/repositories/PersonalRecordRepository.java
+++ src/main/java/repositories/PersonalRecordRepository.java
@@ -3,9 +3,10 @@ package repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import domain.Brand;
+import domain.PersonalRecord;
 
 @Repository
-public interface BrandRepository extends JpaRepository<Brand, Integer> {
+public interface PersonalRecordRepository extends
+		JpaRepository<PersonalRecord, Integer> {
 
 }
